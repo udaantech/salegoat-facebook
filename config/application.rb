b9d15df6cee6ad53253db0c2e90e9cf55e028fb8
Priyank Jain <priyank.jain@webenturetech.com>
2014-08-26 10:09:25 +0530

    bg and font files

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module FacebookAuthExample
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.assets.paths << "#{Rails}/vendor/assets"
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
