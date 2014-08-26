360f04b96205d96d1a106efa76bbbc88a51239a8
Priyank Jain <priyank.jain@webenturetech.com>
2014-08-26 12:34:00 +0530

    fonts

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,11 @@ module FacebookAuthExample
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    # Add the fonts path
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
