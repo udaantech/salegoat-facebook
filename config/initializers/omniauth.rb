0f0440ea017905a099c1dcab0efd4f5add007e61
udaantechmohit <mohit.verma@udaantechnologies.com>
2014-08-28 10:16:49 +0530

    facebook integration

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '428057170545953', '50571ec31ec1a2306558a65b27220649', {:client_options => {:ssl => {:ca_file => Rails.root.join("cacert.pem").to_s}}}
+
 end
