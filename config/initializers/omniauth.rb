86fb27a80bdf9479e774a6a514c701642c12ea2f
Priyank Jain <priyank.jain@webenturetech.com>
2014-09-03 10:14:43 +0530

    change secret key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,9 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env == 'production'
-  provider :facebook, '452181538258114', 'bf105dbd1547dd4414c3b7ceda011cdb', :scope => 'user_groups, publish_actions'
+   if Rails.env == 'production'
+   #if Rails.env == 'development'
+  provider :facebook, '452181538258114', '0cc9bd2cc2491649571bffbdbe3ba699', :scope => 'user_groups, publish_actions'
   else
   provider :facebook, '428057170545953', '50571ec31ec1a2306558a65b27220649', :scope => 'user_groups, publish_actions'
   end	
